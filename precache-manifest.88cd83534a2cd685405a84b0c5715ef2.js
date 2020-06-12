baf842cb11ab75683cfa6d48792aaa1b45b4ae20
toofaniCoder <smartcoder500@gmail.com>
2020-06-12 06:04:01 +0530

    Updates

---
+++ precache-manifest.88cd83534a2cd685405a84b0c5715ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4143dc265f809fec15ed4df6c8a4971",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "9dbbc5b2cd8ee035b833",
+    "url": "/hello-react/static/css/main.3cb97897.chunk.css"
+  },
+  {
+    "revision": "9f6fdf7402e73b120f00",
+    "url": "/hello-react/static/js/2.67f2198d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.67f2198d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbbc5b2cd8ee035b833",
+    "url": "/hello-react/static/js/main.07d93f78.chunk.js"
+  },
+  {
+    "revision": "d2e3280283877fd484a7",
+    "url": "/hello-react/static/js/runtime-main.80cc958b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02614b50001878d011c0b11f9a58a108.js"
+  "/hello-react/precache-manifest.88cd83534a2cd685405a84b0c5715ef2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
