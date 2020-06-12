af519163a37653a4499277166393448b2bfa5bb2
toofaniCoder <smartcoder500@gmail.com>
2020-06-12 05:56:46 +0530

    Updates

---
+++ precache-manifest.02614b50001878d011c0b11f9a58a108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4bdafa0d9ca20eae2a116ad3f59cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d51d671e84d26bb61d4",
+    "url": "/static/css/main.e96abb14.chunk.css"
+  },
+  {
+    "revision": "9f6fdf7402e73b120f00",
+    "url": "/static/js/2.67f2198d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67f2198d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d51d671e84d26bb61d4",
+    "url": "/static/js/main.90113488.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
